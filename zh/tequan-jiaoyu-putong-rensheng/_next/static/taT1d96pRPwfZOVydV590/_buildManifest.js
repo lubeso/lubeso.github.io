6368210f53f241a9deb3781a5fc67269b0221584
lubeso <lubeso@icloud.com>
2022-11-03 01:18:46 -0400

    Remove files

--- zh/tequan-jiaoyu-putong-rensheng/_next/static/taT1d96pRPwfZOVydV590/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-55c2ffaa8365675b.js"],"/_error":["static/chunks/pages/_error-297ad5cd39d4392d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
