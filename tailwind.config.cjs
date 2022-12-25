15cca9902d59d8d104f2b4008e0e6d4a481c76c5
Luis Becerra Solis <lubeso@icloud.com>
2022-12-24 21:19:03 -0500

    Reset: no description

--- tailwind.config.cjs
+++
@@ -1,9 +0,0 @@
-/* eslint-disable no-undef */
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./src/**/*.{astro,md,mdx,ts,tsx}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-};
